67cc95ee23490038cf62c49f3fcd3fe0f6522936
Evgeny Grabun <grabun96@mail.ru>
2022-03-01 17:05:42 +0300

    выполнение 23го урока

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import modal from './modules/modal'
 import scroll from './modules/scroll'
 import reg from './modules/reg'
 import tabs from './modules/tabs'
+import slider from './modules/slider'
 
 timer('28 february 2022')
 menu()
@@ -11,3 +12,4 @@ modal()
 scroll()
 reg()
 tabs()
+slider()
